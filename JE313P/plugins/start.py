6aa54cc9062f8b22f60482218cf8eb6a2a9eee7b
r1004 <101421034+r1004@users.noreply.github.com>
2023-08-07 03:52:18 -0700

    ياعلي مدد

---
+++ JE313P/plugins/start.py
@@ -0,0 +1,47 @@
+from JE313P import JE313P, BOT_USERNAME
+from Config import Config
+from telethon import events, Button
+
+PM_START_TEXT = """
+اهلا بك ! {}
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+✘انا بوت بسيط لحماية مجموعتك وتشغيل المقاطع الصوتية في المكالمه**.
+‣ **استطيع تشغيل المقاطع الصوتية في المكالمة**.
+‣ **استطيع حظر و كتم اي مستخدم**.
+‣ **افضل بوت من ناحية المميزات**
+‣ **يعتمد على مكتبة التيليثون لذلك يكون البوت سريع**!
+‣ **اكتشف الباقي بنفسك**.
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+✘ اضغط على الاسفل لعرض الاوامر الخاصه بي.
+[𖠄 𝗝𝗲𝗽𝘁𝗵𝗼𝗻 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 𖠄](https://t.me/JEPTHON)
+"""
+
+@JE313P.on(events.NewMessage(pattern="^[?!/]start$"))
+async def start(event):
+
+    if event.is_private:
+       await event.client.send_file(event.chat_id,
+             Config.START_IMG,
+             caption=PM_START_TEXT.format(event.sender.first_name), 
+             buttons=[
+        [Button.url("➕ اضغط هنا لأضافتي", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [Button.url("السورس", "https://t.me/JEPTHON")],
+        [Button.url("الدعم", f"https://t.me/{Config.SUPPORT}"), Button.url("القناة", f"https://t.me/{Config.CHANNEL}")],
+        [Button.inline("الاوامر", data="help")]])
+       return
+
+    if event.is_group:
+       await event.reply("**- اهلا بك انا اعمل بنجاح**")
+       return
+
+
+
+@JE313P.on(events.callbackquery.CallbackQuery(data="start"))
+async def _(event):
+    if event.is_private:
+       await event.edit(PM_START_TEXT.format(event.sender.first_name), buttons=[
+        [Button.url("➕ اضغط هنا لاضافتي", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [Button.url("السورس", "https://t.me/JEPTHON")],
+        [Button.url("الدعم", f"https://t.me/{Config.SUPPORT}"), Button.url("القناة", f"https://t.me/{Config.CHANNEL}")],
+        [Button.inline("الاوامر", data="help")]])
+       return
