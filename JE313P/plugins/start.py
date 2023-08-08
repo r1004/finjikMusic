fb06f62b574f8c8248d70658c4ca763540aea073
r1004 <101421034+r1004@users.noreply.github.com>
2023-08-08 04:08:38 +0300

    Update start.py

--- JE313P/plugins/start.py
+++ JE313P/plugins/start.py
@@ -24,7 +24,7 @@ async def start(event):
              caption=PM_START_TEXT.format(event.sender.first_name), 
              buttons=[
         [Button.url("➕ اضغط هنا لأضافتي", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [Button.url("السورس", "https://t.me/JEPTHON")],
+        [Button.url("السورس", "https://t.me/allsayid")],
         [Button.url("الدعم", f"https://t.me/{Config.SUPPORT}"), Button.url("القناة", f"https://t.me/{Config.CHANNEL}")],
         [Button.inline("الاوامر", data="help")]])
        return
